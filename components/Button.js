4f886867a4dd3c4ec79c0dd273adb7d49f7163bb
mishanwong <mishan.wong@gmail.com>
2021-02-08 11:06:17 -0800

    Cleanup files

---
+++ components/Button.js
@@ -0,0 +1,11 @@
+import styles from "../styles/components/Button.module.css";
+
+function Button() {
+  return (
+    <button type="button" className={styles.error}>
+      Destroy
+    </button>
+  );
+}
+
+export default Button;
