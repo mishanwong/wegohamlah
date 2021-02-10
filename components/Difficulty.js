542a46ffebf17392fb148737207d4629add4661a
mishanwong <mishan.wong@gmail.com>
2021-02-10 10:52:57 -0800

    Difficulty color displaying

--- components/Difficulty.js
+++ components/Difficulty.js
@@ -2,7 +2,27 @@ import React from "react";
 import styles from "../styles/components/Difficulty.module.css";
 
 function Difficulty(props) {
-  return <div>{props.difficulty}</div>;
+  const difficulty = props.difficulty;
+
+  if (difficulty === "Hard") {
+    return (
+      <div className={`${styles.difficulty} ${styles.hard}`}>
+        {props.difficulty}
+      </div>
+    );
+  } else if (difficulty === "Moderate") {
+    return (
+      <div className={`${styles.difficulty} ${styles.moderate}`}>
+        {props.difficulty}
+      </div>
+    );
+  } else {
+    return (
+      <div className={`${styles.difficulty} ${styles.easy}`}>
+        {props.difficulty}
+      </div>
+    );
+  }
 }
 
 export default Difficulty;
