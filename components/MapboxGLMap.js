d0afb816f366e0c627e6019570b698d179f744d2
mishanwong <mishan.wong@gmail.com>
2021-02-12 14:33:06 -0800

    Routes card view mobile optimized

--- components/MapboxGLMap.js
+++ components/MapboxGLMap.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
+import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
 import Head from "next/head";
 import styles from "../styles/components/Map.module.css";
 
@@ -23,12 +24,56 @@ const MapboxGLMap = () => {
     });
 
     //Add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl());
 
     map.on("move", () => {
       setLng(map.getCenter().lng.toFixed(4));
       setLat(map.getCenter().lat.toFixed(4));
       setZoom(map.getZoom().toFixed(2));
     });
+
+    //Add cycling route
+    // const directions = new MapboxGLMap({
+    //   // accessToken: process.env.MAPBOX_KEY,
+    //   unit: "metric",
+    //   profile: "mapbox/cycling",
+    //   coordinates: [],
+    // });
+
+    //Add a GeoJSON Line
+    // map.on("load", () => {
+    //   map.addSource("route", {
+    //     type: "geojson",
+    //     data: {
+    //       type: "Feature",
+    //       properties: {},
+    //       geometry: {
+    //         type: "LineString",
+    //         coordinates: [
+    //           [-122.329438, 47.642427],
+    //           [-122.331685, 47.650051],
+    //           [-122.327973, 47.653368],
+    //           [-122.323135, 47.655262],
+    //           [-122.320881, 47.654705],
+    //           [-122.318596, 47.655789],
+    //         ],
+    //       },
+    //     },
+    //   });
+    //   map.addLayer({
+    //     id: "route",
+    //     type: "line",
+    //     source: "route",
+    //     layout: {
+    //       "line-join": "round",
+    //       "line-cap": "round",
+    //     },
+    //     paint: {
+    //       "line-color": "#fc6100",
+    //       "line-width": 8,
+    //     },
+    //   });
+    // });
 
     //Clean up on unmount
     return () => map.remove();
